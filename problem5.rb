570e1f5e2ac671ed616c29759db49e753b51742a
Zorganon <dan.the.hudson@gmail.com>
2016-12-12 21:27:44 -0600

    winning problem 5 configuration

--- problem5.rb
+++ problem5.rb
@@ -7,20 +7,24 @@ def counter
 	x = 2520
 	di = 20
 	flag = 0
-	while flag == 0
+	while flag == 0 do
+		#this while loop should check a given X for factors
 		while di > 3
-			puts "another 18" if di == 18
+			puts "#{x} got another 17" if di == 17
 			if x % di == 0
 				di -= 1
 			else
 				x += 20
+				di = 20
 			end
 		end
+		#^end of while loop
 		if di == 3
 			flag = 1
 			return x
 		end
 	end
+	#^end of while loop
 end
 
 counter
