171ed841ccf965f2e4f885499347699fadc2341e
Zorganon <dan.the.hudson@gmail.com>
2016-12-12 20:32:19 -0600

    solved problem 4

--- problem4.rb
+++ problem4.rb
@@ -1,37 +1,5 @@
 #find the largest palendrome that is the product of two three digit numbers.
 
-def palendrome
-	#999*999 = x
-	# for x backwards to 10000, 
-	# if it's a palendrome run a factoring method
-	# the factoring method checks if it's divisible by a 3 digit number
-	#if so it checks to see the result is also three digits
-
-	# 998,001 - 10,000 is the start of the palendrome quest.
-	palendrom = 0
-
-	for each in (800000...998001)
-		digitstr = each.to_s
-		if (digitstr[0] == digitstr[-1])
-			if (digitstr[1] == digitstr[-2])				
-				if (digitstr[2] == digitstr[-3])
-					palendrom = 1
-					puts digitstr
-				end
-			end					
-		end
-#		#factoring method
-#		if palendrom == 1
-#			for x in ((each/100)..each)
-#				if each % x == 0 && num_digits(x) == 3
-#					return each
-#				end
-#			end
-#		end
-#		sofar = 0
-	end
-end
-
 
 def num_digits(obj)
 	d = 1
@@ -41,6 +9,46 @@ def num_digits(obj)
 		d += 1		
 	end
 	d
+end
+
+def factors(obj)
+	x = (obj/999).round
+	while x < 999
+		if obj % x == 0
+			puts x
+			return true		
+		else
+			x +=1
+		end
+	end
+	return false
+end
+
+
+def palendrome
+	#999*999 = x
+	# for x backwards to 10000, 
+	# if it's a palendrome run a factoring method
+	# the factoring method checks if it's divisible by a 3 digit number
+	#if so it checks to see the result is also three digits
+
+	# 998,001 - 10,000 is the start of the palendrome quest.
+	palendrom = 0
+	factor = false
+
+	for each in (800000..997799)
+		digitstr = each.to_s
+		if (digitstr[0] == digitstr[-1])
+			if (digitstr[1] == digitstr[-2])				
+				if (digitstr[2] == digitstr[-3])
+					if factors(digitstr.to_i) == true
+						puts digitstr
+					end
+				end
+			end					
+		end
+#		sofar = 0
+	end
 end
 
 
